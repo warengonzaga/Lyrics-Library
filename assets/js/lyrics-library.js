c8023225e4631db9de7e938c0cfcecdfbcbe0464
Waren <waren.gonzaga@keydiscoveryinc.com>
2016-01-07 00:11:41 +0800

    Add links for lyrics page of All Tab.

--- assets/js/lyrics-library.js
+++ assets/js/lyrics-library.js
@@ -5,12 +5,37 @@ by Rhen Gonzaga
 
 window.onload = data;
 
+//Page Links
+var loc = "res/lyrics/";
+var type = ".html";
+var aaaa = "fireflies";
+var aaab = "";
+var aaac = "";
+var aaad = "";
+
 //Jquery on Ready
 $(document).ready(function() {
- 
+
  //Fixed Tab Page
  $("#activeTab").click();
- 
+
+     //All Lyrics Page Links
+     $("#lyricsOneOne").click(function () {
+       location.href = loc + aaaa + type;
+     });
+     $("#lyricsOneTwo").click(function () {
+       location.href = loc + aaab + type;
+     });
+     $("#lyricsOneThree").click(function () {
+       location.href = loc + aaac + type;
+     });
+     $("#lyricsOneFour").click(function () {
+       location.href = loc + aaad + type;
+     });
+     $("#lyricsOneFive").click(function () {
+       location.href = loc + aaae + type;
+     });
+
 });
 
 //Touch Events
@@ -18,7 +43,7 @@ $(document).on("pagecreate","#pageEvent",function(){
 
  //Start Up PopUp
  //$("#artist-popup").popup("open");
- 
+
  //Open Panel in Start
  //$("#sidemenu").panel("open");
 
@@ -26,7 +51,7 @@ $(document).on("pagecreate","#pageEvent",function(){
  $("#pageEvent").on("swiperight",function(){
   $("#sidemenu").panel("open");
  });
- 
+
 });
 
 //Tabbed Method
@@ -48,24 +73,8 @@ $("#album").hide();
 $("#artist").fadeIn();
 }
 
+//Application Data
 function data() {
 	var version = "Alpha";
 	document.getElementById("version").innerHTML=version;
-	 $(".se-pre-con").fadeOut("slow");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
